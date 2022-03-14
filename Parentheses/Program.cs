c23870c6fca8686bf4f57f5df62b83bf3d2331dd
OldBeliver <380506338004@yandex.ua>
2022-03-14 15:31:45 +0200

    Добавьте файлы проекта.

---
+++ Parentheses/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Введите строку со скобками: ");
+            string userInput = Console.ReadLine();
+
+            int balance = 0;
+            int depth = 0;
+            bool result = true;
+
+            foreach (var symbol in userInput)
+            {
+                switch (symbol)
+                {
+                    case '(':
+                        balance++;
+                        break;
+                    case ')':
+                        balance--;
+                        break;
+                }
+
+                result = balance >= 0;
+
+                if (result == false)
+                    break;
+
+                if (balance > depth)
+                    depth = balance;
+            }
+
+            if (result && balance == 0)
+            {
+                Console.WriteLine($"скобочное выражение корректное");
+                Console.WriteLine($"максимальная глубина вложения: {depth}");
+            }
+            else
+            {
+                Console.WriteLine($"скобочное выражение не корректное");
+            }
+        }
+    }
+}
